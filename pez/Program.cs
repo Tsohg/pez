58d597934230cd9c91ba31e82f55673b3baa87ef
Tsohg <asaneoil@gmail.com>
2020-04-30 04:01:25 -0400

    bin/exe update

--- pez/Program.cs
+++ pez/Program.cs
@@ -12,14 +12,16 @@ namespace pez
     {
         static void Main(string[] args) //pez filepath -c //for C dispensing on command line.
         {
-            //if (!File.Exists(args[0]))
-            //    throw new Exception("Invalid file path. Must use a full path to the pez source file.");
+            if (!File.Exists(args[0]))
+                throw new Exception("Invalid file path. Must use a full path to the pez source file.");
 
             //might want to check to see if args[1] is a valid file path.
-            
+
+
             //replace first parameter with args[0] when ready for testing.
+            Parser p = new Parser(args[0], args[1], args[2]);
             //Parser p = new Parser(@"C:\Users\Nathan\source\repos\pez\pez\source\pez1.txt", args[1], args[2]);
-            Parser p = new Parser(@"C:\Users\Nathan\source\repos\pez\pez\source\pez1.txt", @"C:\Users\Nathan\source\repos\pez\pez\source\pez2.txt", "c");
+            //Parser p = new Parser(@"C:\Users\Nathan\source\repos\pez\pez\source\pez1.txt", @"C:\Users\Nathan\source\repos\pez\pez\source\pez2.txt", "c");
         }
     }
 }
